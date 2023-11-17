27097ad467834c4da2ff58f11a0ae092171faac2
Tamilarasan Paranthaman <tamilarasan.paranthaman@syncfusion.com>
2023-11-17 19:23:55 +0530

    Initial commit.

---
+++ MauiApp1/Model/OrderInfo.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1
+{
+    public class OrderInfo
+    {
+        private int orderID;
+        private string customerID;
+        private string customer;
+        private string shipCity;
+        private string shipCountry;
+        private double price;
+        private string symbol;
+
+        public int OrderID
+        {
+            get { return orderID; }
+            set { this.orderID = value; }
+        }
+
+        public string CustomerID
+        {
+            get { return customerID; }
+            set { this.customerID = value; }
+        }
+
+        public string ShipCountry
+        {
+            get { return shipCountry; }
+            set { this.shipCountry = value; }
+        }
+
+        public string Customer
+        {
+            get { return this.customer; }
+            set { this.customer = value; }
+        }
+
+        public string ShipCity
+        {
+            get { return shipCity; }
+            set { this.shipCity = value; }
+        }
+
+        public double  Price
+        {
+            get { return price; }
+            set { this.price = value; }
+        }
+
+        public string Symbol
+        {
+            get { return symbol; }
+            set { this.symbol = value; }
+        }
+
+        public OrderInfo(int orderId, string customer, string country,  string customerId, string shipCity, double price, string symbol)
+        {
+            this.OrderID = orderId;
+            this.Customer = customer;
+            this.ShipCountry = country;
+            this.CustomerID = customerId;
+            this.ShipCity = shipCity;
+            this.Price = price;
+            this.symbol = symbol;
+        }
+    }
+}
---
+++ MauiApp1/ViewModel/OrderInfoViewModel.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace MauiApp1
+{
+    public class OrderInfoRepository
+    {
+        private ObservableCollection<OrderInfo> orderInfo;
+        public ObservableCollection<OrderInfo> OrderInfoCollection
+        {
+            get { return orderInfo; }
+            set { this.orderInfo = value; }
+        }
+
+        public OrderInfoRepository()
+        {
+            orderInfo = new ObservableCollection<OrderInfo>();
+            this.GenerateOrders();
+        }
+
+        public void GenerateOrders()
+        {
+            orderInfo.Add(new OrderInfo(1001, "Jack Parker", "Germany", "ALFKI", "Berlin",13000, "triangle"));
+            orderInfo.Add(new OrderInfo(1002, "Maria Anders", "Mexico", "ANATR", "Mexico D.F.",6000, "square"));
+            orderInfo.Add(new OrderInfo(1003, "Alice Johnson", "Mexico", "ANTON", "Mexico D.F.",15000, "triangle"));
+            orderInfo.Add(new OrderInfo(1004, "Thomas Hardy", "UK", "AROUT", "London",8000, "square"));    
+            orderInfo.Add(new OrderInfo(1005, "Emily Davis", "Sweden", "BERGS", "London",7000, "circle"));
+        }
+    }
+}
